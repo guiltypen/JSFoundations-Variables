4c2d8f0115ddab925089373dee84e5f6a90cdfb5
Bader Al-Ramadan <bader.ram@gmail.com>
2021-02-01 19:27:28 +0300

    change values

--- story.js
+++ story.js
@@ -1,8 +1,8 @@
 let nam;
 let age;
 let hobby;
-nam = "Mshary";
-age= 25;
-hobby = "Meditation";
+nam = "Bader";
+age= 26;
+hobby = "Building";
 let age_in_33 = age + 33 
-console.log(`Mr./Mrs. ${nam}, in 33 years, you will be ${age_in_33} years old. By then you ll have decades of ${hobby} practice!`);
+console.log(`Mr./Mrs. ${nam}, in 33 years, you will be ${age_in_33} years old. By then you will have decades of ${hobby} practice!`);
