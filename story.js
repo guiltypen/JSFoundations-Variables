42cffddd7ad7b1784387bac9583817930e98cb21
Bader Al-Ramadan <bader.ram@gmail.com>
2021-02-01 18:53:17 +0300

    03.Variables

---
+++ story.js
@@ -0,0 +1,8 @@
+let nam;
+let age;
+let hobby;
+nam = "Mshary";
+age= 25;
+hobby = "Meditation";
+let age_in_33 = age + 33 
+console.log(`Mr./Mrs. ${nam}, in 33 years, you will be ${age_in_33} years old. By then you ll have decades of ${hobby} practice!`);
